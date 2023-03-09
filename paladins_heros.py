4f9da43f9d774f35deeac78395de24cff1d7846f
bwshn <bekirshn96@gmail.com>
2023-03-09 04:08:15 +0300

    Final commit.

--- paladins_heros.py
+++ paladins_heros.py
@@ -1,4 +1,6 @@
 import re
+from bs4 import BeautifulSoup
+import requests
 
 with open('heros.txt', encoding="utf-8-sig") as f:
     lines = f.readlines()
@@ -9,3 +11,9 @@ characters = []
 for line in lines:
     characters += re.sub('[\[\]\,\"]', "", re.split("=", line)[1].strip()).split()
 
+urls = ["https://paladins.fandom.com/wiki/" + character for character in characters]
+
+result = requests.get(urls[0])
+doc = BeautifulSoup(result.text, "html.parser")
+
+# TODO: keep the scraping for the talents.
